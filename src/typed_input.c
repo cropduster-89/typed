efebfca3b3b4326a6568a28ee1ce20f8f796a029
Tom Blicq <tom.blicq@microlise.com>
2020-01-06 08:48:33 +0000

    fixed return bug

--- src/typed_input.c
+++ src/typed_input.c
@@ -5,12 +5,12 @@ static void IncrementProgressRect(
 	struct entity *progressRect = GetProgressRect(state);
 	struct entity *line = GetCurrentOutputLine(state);
 	char character = line->string.contents[inputControl->string.length - 1].glyph;
-	if(character == '\0') {return;} 
+	if(character == '\0') {return;}
 	else if(character != ' ') {
 		progressRect->dim.x += GetCharacterWidth(state, character);
 	} else {
 		progressRect->dim.x += 10.0f;
-	}	
+	}
 }
 
 static void DeIncrementProgressRect(
@@ -20,39 +20,39 @@ static void DeIncrementProgressRect(
 	struct entity *progressRect = GetProgressRect(state);
 	struct entity *line = GetCurrentOutputLine(state);
 	char character = line->string.contents[inputControl->string.length - 1].glyph;
-	if(character == '\0') {return;} 
+	if(character == '\0') {return;}
 	else if(character != ' ') {
 		progressRect->dim.x -= GetCharacterWidth(state, character);
 	} else {
 		progressRect->dim.x -= 10.0f;
-	}		
+	}
 }
 
 static void TakeInput(
 	struct game_state *state,
 	struct entity *inputControl)
 {
-	inputControl->string.contents[inputControl->string.length++].glyph = 
+	inputControl->string.contents[inputControl->string.length++].glyph =
 		state->input.inputCharacter;
 	if(state->input.inputCharacter != ' ') {
 		inputControl->string.lengthInPixels += GetCharacterWidth(state,
 			state->input.inputCharacter);
 	} else {
 		inputControl->string.lengthInPixels += 10.0f;
-	}	
+	}
 	IncrementProgressRect(state, inputControl);
 }
 
 static void TakeBackspace(
 	struct game_state *state,
 	struct entity *inputControl)
-{	
+{
 	DeIncrementProgressRect(state, inputControl);
 	char removedChar = inputControl->string.
 		contents[inputControl->string.length - 1].glyph;
 	inputControl->string.length--;
-	inputControl->string.lengthInPixels -= GetCharacterWidth(state, removedChar);	
-	BITCLEAR(state->input.state, ISTATE_BACKSPACE);	
+	inputControl->string.lengthInPixels -= GetCharacterWidth(state, removedChar);
+	BITCLEAR(state->input.state, ISTATE_BACKSPACE);
 }
 
 extern void ClearInput(
@@ -71,7 +71,7 @@ static void TakeReturn(
 	state->lettersWrongCarry += state->lettersWrong;
 	state->lettersTypedCarry += state->lettersTyped;
 	struct entity *progressRect = GetProgressRect(state);
-	progressRect->dim.x = 0;	
+	progressRect->dim.x = 0;
 	progressRect->pos.y -= 25.0f;
 	struct entity_event *event = NewEvent(state, progressRect->index, EVENT_MOVEUP);
 		NewMoveEvent(state, event, FloatToVec2(progressRect->pos.x, progressRect->pos.y + 25.0f),
@@ -83,14 +83,14 @@ static void TakeReturn(
 static bool TakingPgUp(
 	struct game_state *state)
 {
-	return(!BITCHECK(state->global, GLOBAL_GAME) && BITCHECK(state->input.state, ISTATE_PGUP) && 
+	return(!BITCHECK(state->global, GLOBAL_GAME) && BITCHECK(state->input.state, ISTATE_PGUP) &&
 		state->atLine > 0 && FreeEvent(state));
 }
 
 static bool TakingPgDn(
 	struct game_state *state)
 {
-	return(!BITCHECK(state->global, GLOBAL_GAME) && BITCHECK(state->input.state, ISTATE_PGDOWN) && 
+	return(!BITCHECK(state->global, GLOBAL_GAME) && BITCHECK(state->input.state, ISTATE_PGDOWN) &&
 		state->atLine < state->outputLines && FreeEvent(state));
 }
 
@@ -106,16 +106,16 @@ static bool TakingReturn(
 	struct game_state *state,
 	struct entity *inputControl)
 {
-	return(BITCHECK(state->global, GLOBAL_GAME) && 
+	return(BITCHECK(state->global, GLOBAL_GAME) &&
 	       BITCHECK(state->input.state, ISTATE_RETURN) &&
-		FreeEvent(state));
+		state->atLine > 0 && FreeEvent(state));
 }
 
 static bool TakingBackspace(
 	struct game_state *state,
 	struct entity *inputControl)
 {
-	return(BITCHECK(state->input.state, ISTATE_BACKSPACE) && 
+	return(BITCHECK(state->input.state, ISTATE_BACKSPACE) &&
 		inputControl->string.length > 0);
 }
 
@@ -125,27 +125,27 @@ static bool ReceiveInput(
 {
 	bool result = false;
 	if(TakingInput(state, inputString)) {
-		result = true;	
+		result = true;
 		if(!BITCHECK(state->global, GLOBAL_GAME)) {
 			NewGame(state);
-		}	
-		TakeInput(state, inputString);				
-	} else if(TakingBackspace(state, inputString)) {	
-		TakeBackspace(state, inputString);	
+		}
+		TakeInput(state, inputString);
+	} else if(TakingBackspace(state, inputString)) {
+		TakeBackspace(state, inputString);
 		result = true;
-	} else if(TakingReturn(state, inputString)) {			
+	} else if(TakingReturn(state, inputString)) {
 		state->atLine++;
 		TakeReturn(state, inputString);
-		ScrollOutput(state, true);					
-	} else if(TakingPgUp(state)) {	
+		ScrollOutput(state, true);
+	} else if(TakingPgUp(state)) {
 		state->atLine--;
-		ScrollOutput(state, false);				
+		ScrollOutput(state, false);
 	} else if(TakingPgDn(state)) {
 		state->atLine++;
-		ScrollOutput(state, true);				
+		ScrollOutput(state, true);
 	} else if(BITCHECK(state->input.state, ISTATE_TAB)) {
 		RestartGame(state);
-	}		
+	}
 	state->input.state = 0;
 	state->input.inputCharacter = '\0';
 	return(result);
@@ -155,7 +155,7 @@ extern void InputControl(
 	struct game_state *state,
 	struct entity *inputControl)
 {
-	if(ReceiveInput(state, inputControl)) {		
-		CompareInput(state, inputControl);		
-	}	
+	if(ReceiveInput(state, inputControl)) {
+		CompareInput(state, inputControl);
+	}
 }
