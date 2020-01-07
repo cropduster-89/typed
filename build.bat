ee644449cb60d584cecddacae92a642567cd200c
cropduster-89 <tblicq@gmx.co.uk>
2020-01-07 21:46:53 +0000

    optimisations on

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-clang -g -gcodeview -Wall -std=c11 -march=native -O0 src/typed_win32.c -o typed.exe -luser32 -lgdi32 -lwinmm
+clang -g -gcodeview -Wall -std=c11 -march=native -O2 src/typed_win32.c -o typed.exe -luser32 -lgdi32 -lwinmm
